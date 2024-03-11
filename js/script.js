906e4b242abaafc906845982825a4792ece46f4f
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-03-11 20:39:08 -0300

    Fixing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("Olá Gerador de Senha")
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordButton = document.querySelector("#generated-password");
+
+// Funções
+// Letras, Números e Símbolos
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+  console.log("teste");
+});
