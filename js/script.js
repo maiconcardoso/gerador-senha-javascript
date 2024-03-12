8680b4bfa09eb387ff748f0cb139ef7e7f2e0f38
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-03-11 21:10:58 -0300

    Finalização do gerador automático de senha

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Seleção de elementos
 const generatePasswordButton = document.querySelector("#generate-password");
-const generatedPasswordButton = document.querySelector("#generated-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
 // Letras, Números e Símbolos
@@ -12,7 +12,42 @@ const getLetterUpperCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+  const symbols = "(){}[]=<>/,.!@#$%&*+-";
+  return symbols[ Math.floor(Math.random() * symbols.length)];
+}
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+  let password = "";
+  const passwordLength = 10;
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  ]
+  for (i=0; i<passwordLength; i = i+4) {
+    generators.forEach(() => {
+      const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+      password += randomValue;
+    });
+  }
+  password = password.slice(0, passwordLength);
+
+  generatedPasswordElement.style.display =  "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
+}
+
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-  console.log("teste");
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  );
 });
